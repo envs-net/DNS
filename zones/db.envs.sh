f6e24822941dbe5e34eb1ca670ece85a3b493594
creme <creme@envs.net>
2021-07-20 20:34:58 +0200

    update zone layout

--- zones/db.envs.sh
+++ zones/db.envs.sh
@@ -1,23 +1,33 @@
 $ORIGIN .
-$TTL 28800  ; 8 hours
-envs.sh         IN SOA  ns1.envs.net. root.envs.net. (
-                2020022701  ; Serial
-                10800       ; Refresh
-                3600        ; Retry
-                604800      ; Expire
-                28800 )     ; Negative Cache TTL
-            NS  ns1.envs.net.
-            NS  ns2.envs.net.
-            NS  ns1.tildeverse.net.
-            A   89.163.145.170
-            CAA 128 issue "letsencrypt.org"
-            SSHFP 1 1 2a3dbba3587d58c0e5ca18538fb740d4d46a147f
-            SSHFP 1 2 edd078ef499fce5ca186daa68e75dc888c69fa358b002898282dc413f6749458
-            SSHFP 3 1 a6af5e859aab4c5aefef799e0b5f4150bfca653a
-            SSHFP 3 2 5340ba48a19751f96f7b5ea6da5e0a58174b2c0456e8ef13886599b17014da2e
-            SSHFP 4 1 26c107e80a42c37290cf17e56571c5d0714da4ba
-            SSHFP 4 2 57e9974ec449fa37c9331c4f943ff6f1da56a2eba7b375aeab0a69bfaca4542f
+$TTL 28800	; 8 hours
+envs.sh			IN SOA	ns1.envs.net. root.envs.net. (
+				2021072012 ; serial
+				10800      ; refresh (3 hours)
+				3600       ; retry (1 hour)
+				604800     ; expire (1 week)
+				28800      ; minimum (8 hours)
+				)
+			NS	ns1.envs.net.
+			NS	ns2.envs.net.
+			NS	ns1.tildeverse.net.
+			A	89.163.145.170
+			SSHFP	1 1 (
+				2A3DBBA3587D58C0E5CA18538FB740D4D46A147F )
+			SSHFP	1 2 (
+				EDD078EF499FCE5CA186DAA68E75DC888C69FA358B00
+				2898282DC413F6749458 )
+			SSHFP	3 1 (
+				A6AF5E859AAB4C5AEFEF799E0B5F4150BFCA653A )
+			SSHFP	3 2 (
+				5340BA48A19751F96F7B5EA6DA5E0A58174B2C0456E8
+				EF13886599B17014DA2E )
+			SSHFP	4 1 (
+				26C107E80A42C37290CF17E56571C5D0714DA4BA )
+			SSHFP	4 2 (
+				57E9974EC449FA37C9331C4F943FF6F1DA56A2EBA7B3
+				75AEAB0A69BFACA4542F )
+			CAA	128 issue "letsencrypt.org"
 $ORIGIN envs.sh.
-*               A       89.163.145.170
-$TTL 120    ; 2 minutes
-_acme-challenge TXT "nagXuRRPXr5qap349w8KtFHbSDOuhseozptmI3_yMG8"
+*			A	89.163.145.170
+$TTL 120	; 2 minutes
+_acme-challenge		TXT	"nagXuRRPXr5qap349w8KtFHbSDOuhseozptmI3_yMG8"
